29546dad0c6c4f93e99cc9da4150d8f33e187b9b
hellono21 <hellono21@gmail.com>
2013-08-08 15:55:04 +0800

    add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,7 @@ describe "UserPages" do
             it { should have_selector('div.pagination')}
 
             it "should list each user" do
-                User.paginate(page: 1) each do |user|
+                User.paginate(page: 1).each do |user|
                     expect(page).to have_selector('li', text: user.name)                    
                 end
             end
@@ -41,7 +41,7 @@ describe "UserPages" do
                 it "should be able to delete another user" do
                     expect do
                         click_link('delete', match: :first)
-                    end.change(User, :count).by(-1)
+                    end.to change(User, :count).by(-1)
 
                 end
                 it {should_not have_link('delete', href: user_path(admin))}
@@ -59,11 +59,21 @@ describe "UserPages" do
     end
     describe "profile page" do
         let(:user) { FactoryGirl.create(:user) }
+        let!(:m1) { FactoryGirl.create(:micropost, user:user, content: "Foo")}
+        let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar")}
 
         before { visit user_path(user)}
 
         it {should have_content(user.name)}
         it {should have_title(user.name)}
+
+        describe "microposts" do
+            it {should have_content(m1.content)}
+            it {should have_content(m2.content)}
+            it { should have_content(user.microposts.count) }
+        end
+
+
     end
 
     describe "signup" do
@@ -82,7 +92,7 @@ describe "UserPages" do
                 fill_in "Name", with: "Example User"
                 fill_in "Email", with: "user@example.com"
                 fill_in "Password", with: "foobar"
-                fill_in "Confirmation", with: "foobar"
+                fill_in "Confirm Password", with: "foobar"
             end
             it "should create a user " do
                 expect { click_button submit }.to change(User, :count).by(1)
